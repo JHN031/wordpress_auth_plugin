1d12dc5dece6a063b3cb6e56579b6a52d80f301a
JHN031 <javad.hamashbouri@gmail.com>
2024-09-04 15:17:51 +0330

    delete install codes

--- authplugin.php
+++ authplugin.php
@@ -9,35 +9,6 @@ Text Domain: auth_plugin
 Domain Path: /languages/
 Version: 1.0.0
  */
-function auth_plugin_install_default_configs()
-{
-    $current_configs = get_option('auth_plugin_configs');
-    if (!$current_configs) {
-        $default_configs = [
-            'amount' => 50000,
-            'role' => 'administrator'
-        ];
-        update_option('auth_plugin_configs' , $default_configs);
-    }
-}
-// function auth_plugin_install_db()
-// {
-//     global $wpdb;
-//     $customers_table_name = $wpdb->prefix . 'customers';
-//     $collate = $wpdb->get_charset_collate();
-//     $customers_table_sql = "
-//         CREATE TABLE '{$}'
-//     ";
-// }
-function auth_plugin_unistall()
-{
-    delete_option('auth_plugin_configs');
-}
-function auth_plugin_activate()
-{
-    auth_plugin_install_default_configs();
-    register_uninstall_hook(__FILE__,'auth_plugin_unistall');
-}
 
 define('APP_AUTH_DIR', plugin_dir_path(__FILE__));
 define('APP_AUTH_URL', plugin_dir_url(__FILE__));
